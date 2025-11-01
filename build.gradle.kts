f51be7a7f29ff48894626a666f0a736f2e22600d
Mio <140413118+MioArchive@users.noreply.github.com>
2025-11-01 17:58:25 +0100

    Update build.gradle.kts: bump Java version to 1.21.8 and upgrade ViaVersion and ViaBackwards to 5.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ tasks {
     // 1.17           = Java 16
     // 1.18 - 1.20.4  = Java 17
     // 1-20.5+        = Java 21
-    val version = "1.21.4"
+    val version = "1.21.8"
     val javaVersion = JavaLanguageVersion.of(21)
 
     val jvmArgsExternal = listOf(
@@ -69,8 +69,8 @@ tasks {
     )
 
     val sharedPlugins = runPaper.downloadPluginsSpec {
-        url("https://github.com/ViaVersion/ViaVersion/releases/download/5.4.2/ViaVersion-5.4.2.jar")
-        url("https://github.com/ViaVersion/ViaBackwards/releases/download/5.4.2/ViaBackwards-5.4.2.jar")
+        url("https://github.com/ViaVersion/ViaVersion/releases/download/5.5.1/ViaVersion-5.5.1.jar")
+        url("https://github.com/ViaVersion/ViaBackwards/releases/download/5.5.1/ViaBackwards-5.5.1.jar")
     }
 
     runServer {
